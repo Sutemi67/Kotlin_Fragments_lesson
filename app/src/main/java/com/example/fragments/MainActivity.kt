dee16ce5c1a6a96db4bb0e4e659dc694f2d76b35
Sergey Boykov <sutemi67@gmail.com>
2024-10-08 13:20:18 +0300

    patch

--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -5,14 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.commit
+import com.example.fragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -20,6 +24,23 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .add(R.id.fragment_container_view, CitiesFragment())
+                .commit()
+        }
+        setOnclickListeners()
+    }
+
+    private fun setOnclickListeners() {
+        binding.replaceCountryFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, CountriesFragment())
+                .setReorderingAllowed(true)
+                .commit()
+        }
+        binding.replaceBackStackCountryFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, CountriesFragment())
+                .addToBackStack("countries")
+                .setReorderingAllowed(true)
                 .commit()
         }
     }
